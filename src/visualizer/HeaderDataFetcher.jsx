d6e18a641017c020379c2ecbb55229fc8eb4044e
Kilian Voss <kilian96@live.de>
2022-01-14 14:31:49 +0800

    Improved the HeaderDataFetcherModule to load Datasources list, AssetPair list and CandleSticks list, added a second state Variable to save User selcetion data

--- src/visualizer/HeaderDataFetcher.jsx
+++ src/visualizer/HeaderDataFetcher.jsx
@@ -1,21 +1,11 @@
 import {useState, useEffect} from 'react';
-import Asset_List from './Asset_List'
-
-
 // Execute Fetch function and set State 
 
 let DataFetcherHeader = (props) => {
    //Neue Position erstelle und zurück schicken
    const [HeaderData, setHeaderData] = useState({Loading: true})
-//    let GetHeaderData = async () => {
-//     setHeaderData({
-//         // HeaderDataFetcher: DataFetcherModule,
-//         // Indicators: IndicatorFetched.Indicators,
-//         // OHLC: OHLCFetched.OHLC,
-//         // Simulation: SimulationFetched.Simulation,
-//         // Loading : false
-//     })
-// }
+   const [UserSelection, setUserSelection] = useState({Asset: 'Select Exchange first', CandleSize: 'Select Asset first'})
+
 //    useEffect(() => {
 //     GetHeaderData()
 //   },[])
@@ -35,25 +25,42 @@ let DataFetcherHeader = (props) => {
     let lol = await fetch('http://localhost:5001/AssetPairs', options)
     let lel = await lol.json()
     setHeaderData({
-        AssetPairs: lel.AssetPairs.assetPairs,
+        assetPairs: lel.AssetPairs.assetPairs,
+        candleSizes: lel.AssetPairs.candleSize,
         Loading : false
     })
      // Set Header Data Here
 
    };
    
-   
-   
     const listDataSources = props.dataSet.Metadata.map((element) => 
         <p onClick={() => send(element)}>{element.name}</p>
     )
     
-    const listAssetPairs = HeaderData.Loading === true ? 
-        <p>Loading</p>  : 
-        <p>{Asset_List(HeaderData)}</p>
- 
+    let listAssetPairs = (props)=>{
+        const AssetPairs = props.assetPairs.map((element) => 
+            <p onClick={() => setUserSelection({Asset: element})}>{element}</p>
+        )
+        return AssetPairs
+    }
 
-    console.log(HeaderData);
+    const AssetPairs = HeaderData.Loading === true ? 
+        <p>{UserSelection.Asset}</p>  : 
+        <p>{listAssetPairs(HeaderData)}</p>
+ 
+    let listCandleSizes = (props)=>{
+        const CandleSizes = props.candleSizes.map((element) => 
+            <p onClick={() => setUserSelection({CandleSize: element})}>{element}</p>
+        )
+        return CandleSizes
+    }
+
+    const CandleSizes = HeaderData.Loading === true ? 
+        <p>{UserSelection.CandleSize}</p>  : 
+        <p>{listCandleSizes(HeaderData)}</p>
+
+
+    console.log(UserSelection);
 
     return(
         <div style={{
@@ -62,18 +69,17 @@ let DataFetcherHeader = (props) => {
         }}>
             <h2 style={{}}>Data Fetcher</h2>
             <ul>
-                <li>
-                    <p style={{textAlign: 'left'}}>
-                        Data-Source: 
-                    </p>
-                    {listDataSources}
+                <li style={{textAlign: 'left'}}>
+                    <p>Data-Source:</p>
+                    <div>{listDataSources}</div>
                 </li>
-                <li>
-                    <p style={{textAlign: 'center'}}>Asset-Pair:</p>
-                    {listAssetPairs}
+                <li style={{textAlign: 'center'}}>
+                    <p>Asset-Pair:</p>
+                    <div>{AssetPairs}</div>
                 </li>
-                <li>
-                    <p style={{textAlign: 'right'}}>Configuration:</p>
+                <li style={{textAlign: 'right'}}>
+                    <p>Candlesize Timeframe:</p>
+                    <div>{CandleSizes}</div>
                 </li>
             </ul>
             
